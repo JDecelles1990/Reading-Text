051e32b662d54ef7b997d52d75dbb8b56afe6227
Jonathan Decelles <Jonathan_Decelles1990@hotmail.com>
2022-11-08 04:44:17 -0500

    Add files via upload

---
+++ threading.py
@@ -0,0 +1,15 @@
+import time
+start = time.perf_counter()
+
+
+def do_something():
+    print('Sleeping 1 sec...')
+    time.sleep(1)
+    print('Done Sleeping...')
+
+
+do_something()
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
